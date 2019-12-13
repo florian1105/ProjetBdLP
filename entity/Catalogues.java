71ec7f085f2af5e982c1bf3d76a6e4380b1f8d6e
florian M <florian110599@gmail.com>
2019-12-13 12:46:12 +0100

    ajout classe entité

---
+++ entity/Catalogues.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class Catalogues {
+
+	public Catalogues() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ entity/Produits.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class Produits {
+
+	public Produits() {
+		
+	}
+
+}
