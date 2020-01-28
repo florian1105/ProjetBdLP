674014b8c63d441989913c8fe707d84fdabc5d76
florian M <florian110599@gmail.com>
2020-01-28 12:45:19 +0100

    ajout adapteur dao xml

---
+++ dao/ProduitDAO_XML_adapteur.java
@@ -0,0 +1,58 @@
+package dao;
+
+import java.util.List;
+
+import entity.I_Produit;
+import exceptions.ExceptionNomProduitIllegal;
+import exceptions.ExceptionPrixProduitIllegal;
+import exceptions.ExceptionQuantiteProduitIllegal;
+
+public class ProduitDAO_XML_adapteur implements I_ProduitDAO {
+	
+	public ProduitDAO_XML daoXml ; 
+	public ProduitDAO_XML_adapteur() {
+		daoXml = new ProduitDAO_XML();
+	}
+
+	@Override
+	public void openConnection() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeConnection() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void create(I_Produit produit) {
+		this.daoXml.creer(produit);
+		
+	}
+
+	@Override
+	public I_Produit find(String nom) {
+		try {
+			return this.daoXml.lire(nom);
+		} catch (NumberFormatException | ExceptionNomProduitIllegal | ExceptionPrixProduitIllegal
+				| ExceptionQuantiteProduitIllegal e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null; 
+		}
+	}
+
+	@Override
+	public List<I_Produit> findAll() {
+		return this.daoXml.lireTous();
+	}
+
+	@Override
+	public void remove(String nomProduit) {
+		this.daoXml.supprimer(find(nomProduit));
+		
+	}
+
+}
